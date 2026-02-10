0b756c9108fc8059db53a1ec24237fb7a3fb0933
Sirwin <sirwin2win@gmail.com>
2026-02-10 11:35:28 +0100

    changes made

--- controller/orderController.js
+++ controller/orderController.js
@@ -4,7 +4,8 @@ const OrderItems = require('../models/orderItemsModel')
 
 
 exports.createOrder = async(req,res)=>{
-    const {totalAmount,userId, items} = req.body
+    const {totalAmount,userId} = req.body
+    // const {totalAmount,userId, items} = req.body
     const orderRef = `ORD-${Date.now()}`
     const order = new Order({
         totalAmount:totalAmount,
@@ -13,29 +14,40 @@ exports.createOrder = async(req,res)=>{
         currency:"NGN",
         status:'pending'
     })
-// orderRef, product_id,product_title,product_quantity,product_price
-    let orderItems;
-    for(let item of items){
-        orderItems = new OrderItems({
-            orderRef: orderRef,
-            productId:item.product_id,
-            productTitle:item.product_title,
-            productQuantity:item.product_quantity,
-            productPrice:item.product_price
-        })
-    }
-
     try {
         const newOrder = await order.save()
-        if(!newOrder){
-            res.send("Sorry, couldn't create order at the moment")
-        }else{
-            const newOrderItems = await orderItems.save()
-            if(newOrderItems){
-                res.send(newOrderItems.orderRef)
-            }
+        if(!order){
+            return res.status(401).json({msg:"Sorry we could not create order"})
         }
+        res.send(newOrder.orderRef)
     } catch (error) {
-        res.status(500).json({success:false, error:error.message})
+        
     }
+
 }
+// orderRef, product_id,product_title,product_quantity,product_price
+    // let orderItems;
+    // for(let item of items){
+    //     orderItems = new OrderItems({
+    //         orderRef: orderRef,
+    //         productId:item.product_id,
+    //         productTitle:item.product_title,
+    //         productQuantity:item.product_quantity,
+    //         productPrice:item.product_price
+    //     })
+    // }
+
+//     try {
+//         const newOrder = await order.save()
+//         if(!newOrder){
+//             res.send("Sorry, couldn't create order at the moment")
+//         }else{
+//             const newOrderItems = await orderItems.save()
+//             if(newOrderItems){
+//                 res.send(newOrderItems.orderRef)
+//             }
+//         }
+//     } catch (error) {
+//         res.status(500).json({success:false, error:error.message})
+//     }
+// }
